af08c2140a2a370290ef3f18a220b596bd0e68c5
sumito3478 <sumito3478@gmail.com>
2014-09-24 00:25:52 +0900

    Add Database#cursor to support AQL Query Cursor

--- core/src/main/scala/db.scala
+++ core/src/main/scala/db.scala
@@ -20,9 +20,38 @@ import connection._
 import collection._
 import scala.language.dynamics
 import scala.concurrent._
+import internal.Implicits._
+import scalaz._, Scalaz._
 
 case class DatabaseInfo(name: String, id: String, path: String, isSystem: Boolean)
 case class User(username: String, passwd: Option[String], active: Boolean, extra: Json)
+case class AQL(source: String)
+case class CursorQuery(query: String, count: Boolean, batchSize: Int)
+case class CursorQueryResult[A](hasMore: Boolean, error: Boolean, id: Option[String], result: List[A], code: Int, count: Option[Int])
+case class Cursor[A](head: List[A], tail: Option[() => Future[Cursor[A]]]) {
+  /**
+   * Applies a binary operator to all elements returned by this cursor, going first to last.
+   */
+  def foldLeft[B](init: B)(f: (B, A) => B): Future[B] = {
+    def loop(c: Cursor[A], acc: B): Future[B] = c.tail match {
+      case None => Future(c.head.foldLeft(acc)(f))
+      case Some(x) =>
+        val h = c.head.foldLeft(acc)(f)
+        for {
+          y <- x()
+          acc <- loop(y, h)
+        } yield acc
+    }
+    loop(this, init)
+  }
+  /**
+   * Read all results from the cursor - this method exists mainly for testing purpose.
+   */
+  def readAll: Future[List[A]] =
+    foldLeft(DList[A]()) {
+      case (acc, x) => acc :+ x
+    }.map(_.toList)
+}
 
 sealed trait DatabaseLike extends Dynamic {
   val connection: Connection
@@ -33,6 +62,22 @@ sealed trait DatabaseLike extends Dynamic {
    * Retrieves information about the current database
    */
   def _info: Future[DatabaseInfo] = (_dispatcher.GET / "database/current").dispatch[DatabaseInfo]()
+
+  /**
+   * Create an AQL query cursor and returns the results as a Cursor object
+   */
+  def cursor[A: UnMarshall](query: AQL, count: Boolean = false, batchSize: Int = 1000): Future[Cursor[A]] = {
+    def fail(): Nothing = throw new ArangoDriverException("hasMore = true but no id found")
+    def nextCursor(prev: CursorQueryResult[A]): Future[Cursor[A]] = {
+      for {
+        next <- (_dispatcher.copy(body = Some(Json.write(marshall(CursorQuery(query = query.source, count = count, batchSize = batchSize))))).PUT
+          / s"cursor/${prev.id.getOrElse(fail())}").dispatchRoot[CursorQueryResult[A]]
+      } yield if (next.hasMore) Cursor(head = next.result, tail = Some(() => nextCursor(next))) else Cursor(head = next.result, tail = None)
+    }
+    for {
+      first <- (_dispatcher.copy(body = Some(Json.write(marshall(CursorQuery(query = query.source, count = count, batchSize = batchSize))))).POST / s"cursor").dispatchRoot[CursorQueryResult[A]]
+    } yield if (first.hasMore) Cursor(head = first.result, tail = Some(() => nextCursor(first))) else Cursor(head = first.result, tail = None)
+  }
 }
 
 case class Database(name: String, connection: Connection) extends DatabaseLike {
---
+++ core/src/test/scala/DBSpec.scala
@@ -0,0 +1,63 @@
+/*
+Copyright 2014 sumito3478 <sumito3478@gmail.com>
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package scarango
+
+import scala.concurrent._, duration._
+import internal.Implicits._
+import collection._
+import json._, Marshall._, UnMarshall._
+import org.scalatest._
+import org.scalatest.concurrent._
+
+class DBSpec extends FunSpec with ScalaFutures with DiagrammedAssertions {
+  case class A(x: Int, y: String, z: Option[Int])
+  case class B(x: Int, y: A, z: Option[A])
+  val a1 = A(1, "1", Some(1))
+  val a2 = A(1, "1", None)
+  val b1 = B(1, a1, Some(a1))
+  val b2 = B(1, a2, None)
+  val b3 = B(2, a1, Some(a2))
+  describe("A DB") {
+    describe("when query with AQL cursor") {
+      it("returns results") {
+        val _system = connection.Connection(executor = http.Executor())._system
+        val testcol = _system.testcol
+        try {
+          val f = for {
+            Document(a1id, _, _) <- testcol.save(doc = a1, createCollection = true, waitForSync = true)
+            Document(a2id, _, _) <- testcol.save(doc = a2, waitForSync = true)
+            Document(b1id, _, _) <- testcol.save(doc = b1, waitForSync = true)
+            Document(b2id, _, _) <- testcol.save(doc = b2, waitForSync = true)
+            Document(b3id, _, _) <- testcol.save(doc = b3, waitForSync = true)
+            a1found <- testcol.document[A](id = a1id)
+            a2found <- testcol.document[A](id = a2id)
+            b1found <- testcol.document[B](id = b1id)
+            b2found <- testcol.document[B](id = b2id)
+            cursor <- _system.cursor[Document](query = db.AQL("FOR x IN testcol RETURN x"), count = true, batchSize = 2)
+            results <- cursor.readAll
+          } yield {
+            val resultsSet = results.map(_._id).toSet
+            assert(resultsSet == Set(a1id, a2id, b1id, b2id, b3id))
+          }
+          val ready = f.isReadyWithin(1.minute)
+          assert(ready)
+        } finally {
+          testcol.delete
+        }
+      }
+    }
+  }
+}
+
