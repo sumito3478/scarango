acff2b9a003a5c80878edcf993e555ff1641cf18
sumito3478 <sumito3478@gmail.com>
2014-10-13 15:18:21 +0900

    Test with a mock LoggerImpl

--- src/test/scala/LoggingSpec.scala
+++ src/test/scala/LoggingSpec.scala
@@ -24,24 +24,57 @@ import org.scalatest.concurrent._
 //import http._, db._, connection._, collection._
 //import json._, Json._
 //import Disposable._
+import scala.collection._
 import logging._
-import logging.Defaults._
 import internal._
 
 class LoggingSpec extends FunSpec with ScalaFutures with DiagrammedAssertions {
   describe("Logger") {
     it("outputs logs") {
-      // TODO: implement a mock LoggerImpl and test with it
+      sealed trait MockLoggerImpl
+      implicit object MockLoggerImpl extends LoggerImpl[MockLoggerImpl] {
+        val traces = new mutable.ListBuffer[(String, String, Option[Throwable])]
+        def isTraceEnabled = true
+        def trace(pos: String, msg: String) = traces += ((pos, msg, None))
+        def traceThrowable(pos: String, msg: String, e: Throwable) = traces += ((pos, msg, Some(e)))
+        val debugs = new mutable.ListBuffer[(String, String, Option[Throwable])]
+        def isDebugEnabled = true
+        def debug(pos: String, msg: String) = debugs += ((pos, msg, None))
+        def debugThrowable(pos: String, msg: String, e: Throwable) = debugs += ((pos, msg, Some(e)))
+        val infos = new mutable.ListBuffer[(String, String, Option[Throwable])]
+        def isInfoEnabled = true
+        def info(pos: String, msg: String) = infos += ((pos, msg, None))
+        def infoThrowable(pos: String, msg: String, e: Throwable) = infos += ((pos, msg, Some(e)))
+        val warns = new mutable.ListBuffer[(String, String, Option[Throwable])]
+        def isWarnEnabled = true
+        def warn(pos: String, msg: String) = warns += ((pos, msg, None))
+        def warnThrowable(pos: String, msg: String, e: Throwable) = warns += ((pos, msg, Some(e)))
+        val errors = new mutable.ListBuffer[(String, String, Option[Throwable])]
+        def isErrorEnabled = true
+        def error(pos: String, msg: String) = errors += ((pos, msg, None))
+        def errorThrowable(pos: String, msg: String, e: Throwable) = errors += ((pos, msg, Some(e)))
+      }
+      val e = new RuntimeException("this is a test exception!")
       Logger.error("test")
-      Logger.errorThrowable("test", new RuntimeException("This is a test exception!"))
+      Logger.errorThrowable("test", e)
       Logger.warn("test")
-      Logger.warnThrowable("test", new RuntimeException("This is a test exception!"))
+      Logger.warnThrowable("test", e)
       Logger.info("test")
-      Logger.infoThrowable("test", new RuntimeException("This is a test exception!"))
+      Logger.infoThrowable("test", e)
       Logger.debug("test")
-      Logger.debugThrowable("test", new RuntimeException("This is a test exception!"))
+      Logger.debugThrowable("test", e)
       Logger.trace("test")
-      Logger.traceThrowable("test", new RuntimeException("This is a test exception!"))
+      Logger.traceThrowable("test", e)
+      def check(x: List[(String, String, Option[Throwable])]) = {
+        assert(MockLoggerImpl.errors.toList.map(_._1).forall(_.startsWith("src/test/scala/LoggingSpec.scala")))
+        assert(MockLoggerImpl.errors.toList.map(_._2) == List("test", "test"))
+        assert(MockLoggerImpl.errors.toList.map(_._3) == List(None, Some(e)))
+      }
+      check(MockLoggerImpl.errors.toList)
+      check(MockLoggerImpl.warns.toList)
+      check(MockLoggerImpl.infos.toList)
+      check(MockLoggerImpl.debugs.toList)
+      check(MockLoggerImpl.traces.toList)
     }
   }
 }
